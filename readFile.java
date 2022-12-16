7dde198e532629123628d322a024319d6c6ebdfe
josh <j.surujpaul22@gmail.com>
2022-12-15 23:06:54 -0500

    Added method to read from quotes.csv

--- readFile.java
+++ readFile.java
@@ -13,6 +13,7 @@ public class readFile extends habitProjectMain{
             String line = inputStream.nextLine();
             while(inputStream.hasNextLine()){
                 line = inputStream.nextLine();
+
                 String[] ary = line.split(",");
                 String activityName = ary[0];
                 String activityDifficulty = ary[1];
@@ -20,8 +21,8 @@ public class readFile extends habitProjectMain{
                 databaseList.add(activityName);
                 difficultyList.add(activityDifficulty);
 
-            }
 
+            }
             inputStream.close();
 
         } catch (FileNotFoundException e) {
@@ -29,5 +30,31 @@ public class readFile extends habitProjectMain{
         }
 
     }
+
+    public static void readQuoteDatabase(){
+        String fileName = "quotes.csv";
+
+        try{
+            Scanner inputStream = new Scanner(new File(fileName));
+            String line = inputStream.nextLine();
+            while(inputStream.hasNextLine()){
+                line = inputStream.nextLine();
+
+                String[] ary = line.split("//");
+                String quote = ary[0];
+                String AuthorOfQuote = ary[1];
+
+                quotes.add(quote);
+                authorOfQuote.add(AuthorOfQuote);
+
+
+            }
+            inputStream.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not Found.");
+        }
+    }
+
 
 }
