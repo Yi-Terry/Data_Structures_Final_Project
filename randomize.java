49f1cfe89ecedc8b30661c43efacad69ef2a8755
josh <j.surujpaul22@gmail.com>
2022-12-19 20:39:20 -0500

    Added ability to add suggested tasks

--- randomize.java
+++ randomize.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class randomize extends habitProjectMain{
     public static void checkIfUserIsGivenQuote(){
         if(!dailyQuoteGiven){
@@ -14,11 +16,32 @@ public class randomize extends habitProjectMain{
         System.out.println("-------------------------------------------------------------------------------------------");
         System.out.println("We suggest adding the following habit: "+ databaseList.get(rand1));
         System.out.println("-------------------------------------------------------------------------------------------");
+       System.out.println("Would you like to add it? Y or N?");
+       Scanner temp = new Scanner(System.in);
+       String input = temp.next();
+       if(input.equalsIgnoreCase("Y")){
+           taskList.add(databaseList.get(rand1));
+           q1.add(databaseList.get(rand1));
+           s1.push(databaseList.get(rand1));
+           int valueOfDifficulty = Integer.parseInt(difficultyList.get(rand1));
+           if(valueOfDifficulty == 1) {
+               pointMap.put(databaseList.get(rand1), easyPoints);
+               System.out.println("Task Added");
+           }else if(valueOfDifficulty ==2){
+               pointMap.put(databaseList.get(rand1), mediumPoints);
+               System.out.println("Task Added");
+           }else{
+               pointMap.put(databaseList.get(rand1), hardPoints);
+               System.out.println("Task Added.");
+           }
+       }else{
+           System.out.println("");
+       }
     }
 
      public static void giveUserQuote(){
          int max = quotes.size();
-         int min = 1;
+         int min = 0;
          int range = max - min + 1;
          int rand2 = (int) (Math.random() * range) + min;
          System.out.println("Quote of the Day: \n"+ quotes.get(rand2)+"\n-"+authorOfQuote.get(rand2));
